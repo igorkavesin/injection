08856864e835c8f3039fa09cd73a25dd5da78be1
igor kavesin <igor.kavesin@cpascharleroi.be>
2021-10-25 15:34:41 +0200

    ajout methode FormGroup

--- injection-angular/src/app/ws/generer-injection-comptable/mois/mois.component.ts
+++ injection-angular/src/app/ws/generer-injection-comptable/mois/mois.component.ts
@@ -5,6 +5,7 @@ import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/materia
 import { FormGroup, FormControl } from '@angular/forms';
 import { AppDateAdapter, APP_DATE_FORMATS } from 'src/app/shared/format-datepicker';
 import { IForm } from '../../../shared/interface/iform';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-mois',
@@ -18,16 +19,38 @@ import { IForm } from '../../../shared/interface/iform';
 })
 export class MoisComponent implements OnInit {
 
-  startDate = new Date(1990, 0, 1);
-  form = new FormGroup({
-    annee: new FormControl(''),
-    mois: new FormControl(''),
-    provisoire: new FormControl(''),
-    id_utilisateur: new FormControl(''),
+  public form = this.fb.group({
+
+    annee: [null, [Validators.required, Validators.pattern('^[0-9]{4}')]],
+    mois: [null, [Validators.required, Validators.pattern('^[0-9]{2}')]],
+    id_utilisateur: [null, [Validators.required, Validators.pattern('^[0-9]{2,10}')]],
+    provisoire: [null],
   });
 
-  constructor() {
+
+  // form = new FormGroup({
+  //   annee: new FormControl(''),
+  //   mois: new FormControl(''),
+  //   provisoire: new FormControl(''),
+  //   id_utilisateur: new FormControl(''),
+  // });
+
+  ngOnInit(): void {
+    // this.form = this.fb.group({
+
+    //   annee: [null, [Validators.required, Validators.pattern('[0-9]')]],
+    //   mois: [null, [Validators.required, Validators.pattern('[0-9]')]],
+    //   id_utilisateur: [null, [Validators.required]],
+    //   provisoire: [null],
+    // });
+     
+
   }
+
+  constructor(
+    private fb: FormBuilder, 
+    public dialog: MatDialog
+  ) { }
 
   public getValueFromInputApi(): IForm {
     return {
@@ -42,7 +65,6 @@ export class MoisComponent implements OnInit {
   }
 
 
-  ngOnInit(): void {
-  }
+   
 
 }
