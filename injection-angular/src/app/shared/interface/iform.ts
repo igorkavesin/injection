b9fd9b2fcbfd098ab081410a28f39e67d7a23aef
igor kavesin <igor.kavesin@cpascharleroi.be>
2021-11-08 15:04:54 +0100

    removed interface check

--- injection-angular/src/app/shared/interface/iform.ts
+++ injection-angular/src/app/shared/interface/iform.ts
@@ -1,10 +1,3 @@
 export interface IForm {
     callValueFromInput: (arg: string) => string;
-  }
-  
-  export interface ISelect {
-    value: string;
-    viewValue: string;
-    checked?: boolean;
-    check?: string;
-  }
+}
