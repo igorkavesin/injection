72cad33718cfee5d15bed4c87a73b88ba956234f
igor kavesin <igor.kavesin@cpascharleroi.be>
2021-11-08 10:34:11 +0100

    added modules http,common, local and etc...

--- injection-angular/src/app/app-routing.module.ts
+++ injection-angular/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SemaineComponent } from './ws/generer-injection-comptable/semaine/semaine.component';
-import { MoisComponent } from './ws/generer-injection-comptable/mois/mois.component';
+import { VerrouillageDesPaiementsComponent } from './ws/generer-injection-comptable/verrouillage-des-paiements/verrouillage-des-paiements.component';
 import { HomeComponent } from './home/home.component';
 import { GenererInjectionComptableComponent } from './ws/generer-injection-comptable/generer-injection-comptable.companent';
 
